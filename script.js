3605afdc69e5c768c8e6fb4a1d66b78a09eb1a63
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2025-05-30 23:36:45 -0600

    Successfully learned how to use HTML templates and slots

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-class RefImage {
-    constructor(filename, title, description, date, artistName, artistProfile, altText, spoilerReason) {
-        this.filename = filename
-        this.title = title
-        this.description = description
-        this.date = date
-        this.artistName = artistName
-        this.artistProfile = artistProfile
-        this.altText = altText
-        this.spoilerReason = spoilerReason
-    }
-}
+// class RefImage {
+//     constructor(filename, title, description, date, artistName, artistProfile, altText, spoilerReason) {
+//         this.filename = filename
+//         this.title = title
+//         this.description = description
+//         this.date = date
+//         this.artistName = artistName
+//         this.artistProfile = artistProfile
+//         this.altText = altText
+//         this.spoilerReason = spoilerReason
+//     }
+// }
 
-CustomElementRegistry.define("ref", RefImage, HTMLElement)
+// CustomElementRegistry.define("ref", RefImage, HTMLElement)
 
-await fetch("./images.json")
+customElements.define(
+    "ref-image",
+    class extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById("ref-template");
+            let templateContent = template.content;
+
+            const shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(templateContent.cloneNode(true));
+        }
+    },
+);
+
+fetch("/images.json")
 .then(res => res.json())
 .then(data => {
     // do epic shit
+    // let template = document.getElementById("ref-template");
+    let ref = document.createElement("ref-image")
+    let slot = document.createElement("span")
+    slot.setAttribute("slot", "title")
+    slot.appendChild(document.createTextNode("now this is a slotted text"))
+    ref.appendChild(slot)
+    document.getElementById("ref-list").appendChild(ref)
 })
