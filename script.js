107737fc12bc3602c819959382f44058fc35a395
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2025-06-02 20:59:54 -0600

    page is now dynamically populated

--- script.js
+++ script.js
@@ -33,80 +33,76 @@ let url = window.location.href
 let regex = new RegExp("(?:"+ HOST + ")(.*)")
 let page = url.match(regex)[1] // 0th index should be entire url match, 1st index should be only the part after the host (eg: "paprika/main.html")
 
-// fetch("/images.json")
-// .then(res => res.json())
-// .then(data => {
-//     // do epic shit
-//     // let template = document.getElementById("ref-template");
-//     // let ref = document.createElement("ref-card")
-//     // let slot = document.createElement("span")
-//     // slot.setAttribute("slot", "title")
-//     // slot.appendChild(document.createTextNode("now this is a slotted text"))
-//     // ref.appendChild(slot)
-//     // document.getElementById("ref-list").appendChild(ref)
+fetch("/images.json")
+.then(res => res.json())
+.then(data => {
+    // do epic shit
+    // let template = document.getElementById("ref-template");
+    // let ref = document.createElement("ref-card")
+    // let slot = document.createElement("span")
+    // slot.setAttribute("slot", "title")
+    // slot.appendChild(document.createTextNode("now this is a slotted text"))
+    // ref.appendChild(slot)
+    // document.getElementById("ref-list").appendChild(ref)
 
-//     let imageArray
-//     let folderPath
-//     switch (page) {
-//         case "paprika/main.html":
-//             imageArray = data.paprika.main
-//             folderPath = "/images/paprika/main/"
-//             break;
-//         case "paprika/alternative.html":
-//             imageArray = data.paprika.alternative
-//             folderPath = "/images/paprika/alternative/"
-//             break;
-//         case "paprika/other.html":
-//             imageArray = data.paprika.other
-//             folderPath = "/images/paprika/other/"
-//             break;
-//         case "capri.html":
-//             imageArray = data.capri
-//             folderPath = "/images/capri/"
-//             break;
-//         case "other.html":
-//             imageArray = data.other
-//             folderPath = "/images/other/"
-//     }
+    let imageArray
+    let folderPath
+    switch (page) {
+        case "paprika/main.html":
+            imageArray = data.paprika.main
+            folderPath = "/images/paprika/main/"
+            break;
+        case "paprika/alternative.html":
+            imageArray = data.paprika.alternative
+            folderPath = "/images/paprika/alternative/"
+            break;
+        case "paprika/other.html":
+            imageArray = data.paprika.other
+            folderPath = "/images/paprika/other/"
+            break;
+        case "capri.html":
+            imageArray = data.capri
+            folderPath = "/images/capri/"
+            break;
+        case "other.html":
+            imageArray = data.other
+            folderPath = "/images/other/"
+    }
 
-//     for (let index = 0; index < imageArray.length; index++) {
-//         const element = imageArray[index];
-//         const refContainer = document.getElementById("ref-template").content.firstElementChild.cloneNode(true)
-        
-//         let filename = document.createElement("p")
-//         let filenameText = document.createTextNode(element.filename)
-//         filename.className = "ref-filename"
-//         filename.appendChild(filenameText)
-//         refContainer.appendChild(filename)
+    for (let index = 0; index < imageArray.length; index++) {
+        const element = imageArray[index];
+        const refContainer = document.getElementById("ref-template").content.firstElementChild.cloneNode(true)
 
-//         let image = document.createElement("img")
-//         filename.className = "ref-image"
-//         image.setAttribute("src", folderPath + element.filename)
-//         image.setAttribute("alt", element.altText)
-//         refContainer.appendChild(image)
+        let image = refContainer.getElementsByClassName("ref-image")[0]
+        image.setAttribute("src", folderPath + element.filename)
+        image.setAttribute("alt", element.altText)
 
-//         let description = document.createElement("p")
-//         let descriptionText = document.createTextNode(element.description)
-//         description.className = "ref-description"
-//         description.appendChild(descriptionText)
-//         refContainer.appendChild(description)
+        let date = refContainer.getElementsByClassName("ref-date")[0]
+        date.textContent = element.date
 
-//         let date = document.createElement("p")
-//         let dateText = document.createTextNode(element.date)
-//         date.className = "ref-date"
-//         date.appendChild(dateText)
-//         refContainer.appendChild(date)
+        let filename = refContainer.getElementsByClassName("ref-filename")[0]
+        filename.textContent = element.filename
 
-//         document.getElementById("ref-list").appendChild(refContainer)
+        let description = refContainer.getElementsByClassName("ref-description")[0]
+        description.textContent = element.description
 
-//         // let refCard = document.createElement("ref-card")
-//         // console.log(refCard.content)
-//         // let filename = document.createElement("span")
-//         // let filenameText = document.createTextNode(element.filename)
-//         // filename.setAttribute("slot", "filename")
-//         // filename.appendChild(filenameText)
-//         // let refContainer = refCard.getElementsByClassName("ref-container")[0]
-//         // refContainer.getElementsByClassName("filename")[0].appendChild(filename)
-//         // document.getElementById("ref-list").appendChild(refCard)
-//     }
-// })
+        let artistName = refContainer.getElementsByClassName("ref-artist-name")[0]
+        artistName.textContent = element.artistName
+
+        let artistProfile = refContainer.getElementsByClassName("ref-artist-profile")[0]
+        artistProfile.textContent = element.artistProfile
+        artistProfile.setAttribute("href", element.artistProfile)
+
+        document.getElementById("ref-list").appendChild(refContainer)
+
+        // let refCard = document.createElement("ref-card")
+        // console.log(refCard.content)
+        // let filename = document.createElement("span")
+        // let filenameText = document.createTextNode(element.filename)
+        // filename.setAttribute("slot", "filename")
+        // filename.appendChild(filenameText)
+        // let refContainer = refCard.getElementsByClassName("ref-container")[0]
+        // refContainer.getElementsByClassName("filename")[0].appendChild(filename)
+        // document.getElementById("ref-list").appendChild(refCard)
+    }
+})
